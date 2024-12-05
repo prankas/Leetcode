002d53989dc1c02af5549585c0b8eeec63971e95
prankas <126498373+prankas@users.noreply.github.com>
2024-12-05 11:07:44 +0530

    Time: 22 ms (32.76%), Space: 34.7 MB (17.12%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -2,31 +2,19 @@ class Solution {
 public:
     bool canChange(string start, string target) {
         int n=start.length();
-        int j=0,cnt=0;
+        int i=0,j=0,cnt=0,cnt1=0;
         vector<int> let(n,0);
-        for(int i=0;i<n;i++){
-            if(start[i]!='_'){
-                cnt++;
-                while(j<n && target[j]=='_') j++;
-                if(j>=n || target[j]!=start[i]) return 0;
-                j++;
-            }
-            let[i]=cnt;
-        }
-        while(j<n && target[j]=='_') j++;
-        if(j<n) return 0;
-        cnt=0;
         vector<int> lett(n,0);
         for(int i=0;i<n;i++){
+            if(start[i]!='_') cnt1++;
             if(target[i]!='_') cnt++;
-            lett[i]=cnt;
+            lett[i]=cnt,let[i]=cnt1;
         }
-        int i=0;
-        j=0;
         while(i<n && j<n){
             if(start[i]=='_') i++;
             if(target[j]=='_') j++;
             if(start[i]!='_' && target[j]!='_'){
+                if(start[i]!=target[j]) return 0;
                 if(start[i]=='L'){
                     if(((i+1)-let[i])<((j+1)-lett[j])) return 0;
                 } else if(start[i]=='R'){
@@ -37,6 +25,9 @@ public:
                 i++,j++;
             }
         }
+        while(i<n && start[i]=='_') i++;
+        while(j<n && target[j]=='_') j++;
+        if(i<n || j<n) return 0;
         return 1;
     }
 };
