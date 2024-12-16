9556554bb8ab7446eb8fc3aef4874c34445c2a8b
prankas <126498373+prankas@users.noreply.github.com>
2024-12-16 17:05:50 +0530

    Time: 98 ms (21.12%), Space: 50.6 MB (9.18%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>> graph(n+1);
+        for(auto i:times){
+            int u=i[0],v=i[1],t=i[2];
+            graph[u].push_back({v,t});
+        }
+        vector<int> min_time(n+1,INT_MAX);
+        min_time[0]=1;
+        set<pair<int,int>> st;
+        st.insert({0,k});
+        while(!st.empty()){
+            auto it=*st.begin();
+            st.erase(it);
+            if(it.first>=min_time[it.second]) continue;
+            min_time[it.second]=it.first;
+            for(auto i:graph[it.second]){
+                if(min_time[i.first]<=it.first+i.second) continue;
+                st.insert({it.first+i.second,i.first});
+            }
+        }
+        int ans=*max_element(min_time.begin(),min_time.end());
+        return (ans==INT_MAX?-1:ans);
+    }
+};
