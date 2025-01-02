db54caca6d5a2a91dc3f4c82129f8266ad46db03
prankas <126498373+prankas@users.noreply.github.com>
2025-01-02 20:04:13 +0530

    Time: 0 ms (100%), Space: 9.9 MB (99.52%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
+    int char2val(char c){
+        if(c=='A') return 0;
+        if(c=='C') return 1;
+        if(c=='G') return 2;
+        if(c=='T') return 3;
+        return 0;
+    }
     vector<string> findRepeatedDnaSequences(string s) {
-        map<string,int> st;
-        string sub="";
-        if(s.length()<10) return {};
+        if(s.size() <= 10) return {};
         vector<string> ans;
-        for(int i=0;i<10;i++) sub+=s[i];
-        st[sub]++;
+        bitset<1<<20> s1,s2;
+
+        int val=0;
+        for(int i=0;i<10;i++){
+            val = (val<<2)|char2val(s[i]);
+        }
+        s1.set(val);
+
+        int mask = (1<<20)-1;
         for(int i=10;i<s.length();i++){
-            sub.erase(0,1);
-            sub+=s[i];
-            if(st[sub]==1) ans.push_back(sub);
-            st[sub]++;
+            val = ((val<<2) & mask)| char2val(s[i]);
+            if(s2[val]) continue;
+            if(s1[val]){
+                ans.push_back(s.substr(i-10+1,10));
+                s2.set(val);
+            } else
+            s1.set(val);
         }
         return ans;
     }
