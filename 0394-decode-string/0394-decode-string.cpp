bc5a60a28099640d1dfbe164bf3d853b67768d35
prankas <126498373+prankas@users.noreply.github.com>
2025-01-09 18:01:53 +0530

    Time: 0 ms (100%), Space: 9.8 MB (20.35%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -2,51 +2,34 @@ class Solution {
 public:
     string decodeString(string s) {
         stack<string> st;
-        int n = s.length(), i = s.length() - 1, num = 0;
-        string ans = "";
-        st.push(ans);
-        while (i >= 0) {
-            if (s[i] >= '0' && s[i] <= '9') {
-                string nums = "";
-                while (i >= 0 && s[i] >= '0' && s[i] <= '9') {
-                    nums += s[i];
-                    i--;
+        for(int i=0; i<s.length(); i++) {
+            if(s[i] == ']') {
+                string ans = "";
+                while(st.top() != "[") {
+                    ans = st.top() + ans;
+                    st.pop();
                 }
-                reverse(nums.begin(), nums.end());
-                int num = 0;
-                for (auto i : nums) {
-                    num *= 10;
-                    num += i - '0';
-                }
-                string p = st.top();
                 st.pop();
-                string q = st.top();
-                st.pop();
-                while (num)
-                    q.append(p), num--;
-                st.push(q);
-            } else if (s[i] == ']') {
-                i--;
-                string p = "";
-                while (i >= 0) {
-                    if (s[i] == '[' || s[i] == ']')
-                        break;
-                    p += s[i];
-                    i--;
+                string times = "";
+                while(!st.empty() && isdigit(st.top()[0])) {
+                    times = st.top() + times;
+                    st.pop();
                 }
-                st.push(p);
-            } else if (s[i] == '[') {
-                i--;
+                int to_times = stoi(times);
+                string ret = "";
+                while(to_times--) {
+                    ret = ans + ret;
+                }
+                st.push(ret);
             } else {
-                string q = st.top();
-                st.pop();
-                q += s[i];
-                st.push(q);
-                i--;
+                st.push(string(1, s[i]));
             }
         }
-        ans = st.top();
-        reverse(ans.begin(), ans.end());
-        return ans;
+        string res = "";
+        while(!st.empty()) {
+            res = st.top() + res;
+            st.pop();
+        }
+        return res;
     }
 };
