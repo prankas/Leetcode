d1304bd585d47934ec549ef8864deaec9b97ae62
prankas <126498373+prankas@users.noreply.github.com>
2025-01-12 18:17:42 +0530

    Time: 437 ms (5.84%), Space: 184.1 MB (8.88%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -3,24 +3,17 @@ public:
     bool canBeValid(string s, string locked) {
         int n=s.length();
         if(n%2) return 0;
-        vector<int> dp(n+1,0),dp1(n+1,0);
+        vector<vector<int>> dp(n+1,vector<int> (2,0));
         for(int i=n-1;i>=0;i--){
             if(locked[i]=='0'){
-              dp[i]=max(0,dp[i+1]-1);
+              dp[i][0]=max(0,dp[i+1][0]-1);
+              dp[i][1]=dp[i+1][1]-1;
             } else{
-                if(s[i]=='(')  dp[i]=max(0,dp[i+1]-1);
-                else dp[i]=dp[i+1]+1;
+                if(s[i]=='(')  dp[i][0]=max(0,dp[i+1][0]-1),dp[i][1]=dp[i+1][1]+1;
+                else dp[i][0]=dp[i+1][0]+1,dp[i][1],dp[i][1]=dp[i+1][1]-1;
+                if(dp[i][1]>0) return 0;    
             }
         }
-        for(int j=1;j<=n;j++){
-         int i=j-1;
-            if(locked[i]=='0'){
-              dp1[j]=max(0,dp1[j-1]-1);
-            } else{
-                if(s[i]==')')  dp1[j]=max(0,dp1[i]-1);
-                else dp1[j]=dp1[i]+1;
-            }
-        }
-        return (dp[0]==0 && dp1[n]==0);
+        return (dp[0][0]==0);
     }
 };
