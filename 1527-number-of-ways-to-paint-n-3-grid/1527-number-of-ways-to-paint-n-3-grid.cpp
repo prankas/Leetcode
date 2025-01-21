8c58ada1b16283eed890d64d7b220edee938875b
prankas <126498373+prankas@users.noreply.github.com>
2025-01-21 14:42:50 +0530

    Time: 14 ms (48.85%), Space: 13.7 MB (39.63%) - LeetHub

--- 1527-number-of-ways-to-paint-n-3-grid/1527-number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/1527-number-of-ways-to-paint-n-3-grid.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     using ll=long long;
     int mod=1e9+7;
-    int m=0;
-    map<pair<ll,ll>,ll> mp; 
     ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
     ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
     ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-    int dfs(int i,int p){
-        if(mp.find({i,p})!=mp.end()) return mp[{i,p}];
-        if(i==m) return 1;
-        if(i==0) return mp[{i,p}]=mod_add(mod_mul(6,dfs(i+1,0),mod),mod_mul(6,dfs(i+1,1),mod),mod);
-        else{
-            if(p==0) return mp[{i,p}]=mod_add(mod_mul(3,dfs(i+1,0),mod),mod_mul(2,dfs(i+1,1),mod),mod);
-            else return mp[{i,p}]=mod_add(mod_mul(2,dfs(i+1,0),mod),mod_mul(2,dfs(i+1,1),mod),mod);
-        }
-    }
     int numOfWays(int n) {
-        m=n;
-        return dfs(0,-1);
+        vector<vector<int>> dp(n,vector<int> (2,1));
+        dp[0][0]=6,dp[0][1]=6;
+        for(int i=1;i<n;i++){
+            dp[i][0]=mod_add(mod_mul(dp[i-1][0],3,mod),mod_mul(dp[i-1][1],2,mod),mod);
+            dp[i][1]=mod_add(mod_mul(dp[i-1][0],2,mod),mod_mul(dp[i-1][1],2,mod),mod);
+        }
+        return mod_add(dp[n-1][0],dp[n-1][1],mod);
     }
 };
