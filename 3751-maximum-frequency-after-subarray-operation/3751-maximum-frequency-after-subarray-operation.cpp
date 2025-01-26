ef9bf8218df7372a013a0367a5303fe5984a731e
prankas <126498373+prankas@users.noreply.github.com>
2025-01-26 18:05:29 +0530

    Time: 35 ms (72.22%), Space: 100.7 MB (50%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.cpp
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.cpp
@@ -4,13 +4,11 @@ public:
         int n=nums.size(),m=1,ans=0;
         map<int,int> mp;
         vector<int> dp(n,0);
-        dp[0]=1;
-        mp[nums[0]]=0;
+        dp[0]=1,mp[nums[0]]=0;
         for(int i=1;i<n;i++){
-            int p=0;
-            if(mp.find(nums[i])!=mp.end()) p=max(p,dp[mp[nums[i]]]);
-            if(mp.find(k)!=mp.end()) p=max(p,dp[mp[k]]);
-            dp[i]=p+1;
+            if(mp.find(nums[i])!=mp.end()) dp[i]=dp[mp[nums[i]]];
+            if(mp.find(k)!=mp.end()) dp[i]=max(dp[i],dp[mp[k]]);
+            dp[i]++;
             mp[nums[i]]=i;
             if(nums[i]==k) m++;
             ans=max({ans,m,dp[i]});
