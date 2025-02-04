a335b63864914b5401546ad7b8e8373423e5306d
prankas <126498373+prankas@users.noreply.github.com>
2025-02-04 10:55:01 +0530

    Time: 53 ms (54.17%), Space: 33 MB (37.5%) - LeetHub

--- 1389-minimum-moves-to-move-a-box-to-their-target-location/1389-minimum-moves-to-move-a-box-to-their-target-location.cpp
+++ 1389-minimum-moves-to-move-a-box-to-their-target-location/1389-minimum-moves-to-move-a-box-to-their-target-location.cpp
@@ -1,78 +1,57 @@
-struct State {
-    int bx, by, px, py, pushes;
-};
- 
 class Solution {
 public:
+    using State=tuple<int,int,int,int,int>;
     int minPushBox(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        pair<int,int> target, box, player;
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if(grid[i][j]=='T') target = {i,j};
-                else if(grid[i][j]=='B') box = {i,j};
-                else if(grid[i][j]=='S') player = {i,j};
+        int n=grid.size(),m=grid[0].size();
+        int bx,by,sx,sy,tx,ty;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='B') bx=i,by=j;
+                else if(grid[i][j]=='T') tx=i,ty=j;
+                else if(grid[i][j]=='S') sx=i,sy=j;
             }
         }
-        // Heuristic: Manhattan distance from box to target.
-        auto heuristic = [&](int bx, int by) {
-            return abs(bx - target.first) + abs(by - target.second);
-        };
-        
-        // Comparator for A* using f = pushes + h.
-        auto cmp = [&](const State &a, const State &b) {
-            return a.pushes + heuristic(a.bx, a.by) > b.pushes + heuristic(b.bx, b.by);
-        };
-        priority_queue<State, vector<State>, decltype(cmp)> pq(cmp);
-        
-        // visited[bx][by][px][py]
-        bool visited[21][21][21][21] = {};
-        pq.push({box.first, box.second, player.first, player.second, 0});
-        visited[box.first][box.second][player.first][player.second] = true;
- 
-        int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
- 
-        auto isValid = [&](int x, int y) {
-            return x>=0 && x<m && y>=0 && y<n && grid[x][y] != '#';
-        };
- 
-        auto canReach = [&](int sx, int sy, int tx, int ty, int bx, int by) -> bool {
-            vector<vector<bool>> seen(m, vector<bool>(n,false));
-            queue<pair<int,int>> q;
-            q.push({sx,sy});
-            seen[sx][sy] = true;
-            while(!q.empty()){
-                auto [x,y] = q.front(); q.pop();
-                if(x==tx && y==ty) return true;
-                for(auto &d : dirs){
-                    int nx = x+d[0], ny = y+d[1];
-                    if(isValid(nx,ny) && !seen[nx][ny] && !(nx==bx && ny==by)){
-                        seen[nx][ny] = true;
-                        q.push({nx,ny});
-                    }
+        queue<State> q;
+        bool visited[21][21][21][21]={};
+        q.push({bx,by,sx,sy,0});
+        visited[bx][by][sx][sy]=1;
+        int dirs[4][2]={{0,1},{1,0},{-1,0},{0,-1}};
+        auto canReach = [&](int bx,int by,int ex,int ey,int sx,int sy)->bool{
+           vector<vector<bool>> seen(n,vector<bool> (m,false));
+           queue<pair<int,int>> qq;
+           qq.push({sx,sy});
+           seen[sx][sy]=1;
+           while(!qq.empty()){
+            auto [x,y]=qq.front();
+            qq.pop();
+            if(x==ex && y==ey) return 1;
+            for(auto &d:dirs){
+                int nx=x+d[0],ny=y+d[1];
+                if(nx==bx && ny==by) continue;
+                if(nx>=0 && ny>=0 && nx<n && ny<m && !seen[nx][ny] && grid[nx][ny]!='#'){
+                    seen[nx][ny]=1;
+                    qq.push({nx,ny});
                 }
             }
-            return false;
+           }
+           return false;
         };
- 
-        while(!pq.empty()){
-            auto [cbx, cby, cpx, cpy, pushes] = pq.top();
-            pq.pop();
-            if(cbx==target.first && cby==target.second)
-                return pushes;
- 
-            for(auto &d : dirs){
-                int nbx = cbx + d[0], nby = cby + d[1];   // new box position if pushed
-                int reqX = cbx - d[0], reqY = cby - d[1];   // player must be here to push
-                if(!isValid(nbx, nby) || !isValid(reqX, reqY)) continue;
-                if(!canReach(cpx, cpy, reqX, reqY, cbx, cby)) continue;
-                if(!visited[nbx][nby][cbx][cby]){
-                    visited[nbx][nby][cbx][cby] = true;
-                    pq.push({nbx, nby, cbx, cby, pushes+1});
-                }
+        while(!q.empty()){
+            auto [cbx,cby,cpx,cpy,pushes]=q.front();
+            q.pop();
+            if(cbx==tx && cby==ty) return pushes;
+            for(auto &d:dirs){
+               int nbx=cbx+d[0],nby=cby+d[1];
+               int reqx=cbx-d[0],reqy=cby-d[1];
+               if(nbx<0 || nby<0 || nbx>=n || nby>=m || grid[nbx][nby]=='#') continue;
+               if(reqx<0 || reqy<0 || reqx>=n || reqy>=m || grid[reqx][reqy]=='#') continue;
+               if(!canReach(cbx,cby,reqx,reqy,cpx,cpy)) continue;
+               if(!visited[nbx][nby][cbx][cby]){
+                visited[nbx][nby][cbx][cby]=1;
+                q.push({nbx,nby,cbx,cby,pushes+1});
+               }
             }
         }
- 
         return -1;
     }
 };
