e7e8bc3b04e5789421b9dc124444f04a03898ad6
prankas <126498373+prankas@users.noreply.github.com>
2025-02-04 23:53:07 +0530

    Time: 14 ms (67.4%), Space: 20.2 MB (48.89%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    bool isPalindrome(string &s){
-        string p=s;
-        reverse(p.begin(),p.end());
-        if(p==s) return true;
-        return false;
-    }
-
-    int chk(int mid,string &s){
-        int n=s.length();
-        for(int L=0;L+mid<=n;L++){
-            string p=s.substr(L,mid);
-            if(isPalindrome(p)) return L;
-        }
-        return -1;
-    }
-
     string longestPalindrome(string s) {
         int n=s.length();
         int best_len=0;
         string best_s="";
-        for(int parity:{0,1}){
-            int low=1,high=n;
-            if(low%2 != parity) low++;
-            if(high%2 != parity) high--;
-            while(low<=high){
-                int mid=(low+high)/2;
-                if(mid%2 != parity) mid++;
-                if(mid>high) break;
-                int tmp=chk(mid,s);
-                if(tmp != -1){
-                    if(best_len<mid){
-                    best_len=mid;
-                    best_s=s.substr(tmp,mid);
-                    }
-                    low=mid+2;
-                } else high=mid-2;
+        for(int mid=0;mid<n;mid++){
+            for(int x=0;mid-x>=0 && mid+x<n;x++){
+                if(s[mid-x]!=s[mid+x]) break;
+                int len=2*x+1;
+                if(len>best_len){
+                    best_len=len;
+                    best_s=s.substr(mid-x,len);
+                }
+            }
+        }
+        for(int mid=0;mid<n;mid++){
+            for(int x=1;mid-x+1>=0 && mid+x<n;x++){
+                if(s[mid-x+1]!=s[mid+x]) break;
+                int len=2*x;
+                if(len>best_len){
+                    best_len=len;
+                    best_s=s.substr(mid-x+1,len);
+                }
             }
         }
         return best_s;
