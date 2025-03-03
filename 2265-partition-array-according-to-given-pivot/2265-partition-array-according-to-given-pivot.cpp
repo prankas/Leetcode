7b62fda619ef65438af9c9ef864668c14a366d5b
prankas <126498373+prankas@users.noreply.github.com>
2025-03-03 15:19:58 +0530

    Time: 8 ms (50.55%), Space: 132.7 MB (68.31%) - LeetHub

---
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        auto it = stable_partition(nums.begin(), nums.end(), [pivot](int x) {
+        return x < pivot;
+       });
+    // Then, on the remaining segment, stable partition so that elements equal to pivot come first.
+     stable_partition(it, nums.end(), [pivot](int x) {
+        return x == pivot;
+      });
+    return nums;
+    }
+};
