a1e5114e956c908a6ecfed36afc3953cf0b30db6
prankas <126498373+prankas@users.noreply.github.com>
2025-04-01 23:15:26 +0530

    Time: 1 ms (92.11%), Space: 74.8 MB (27.86%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        int i=0,j=0,ans=0,odd_count=0;
-        int p=0;
-        while(i<n && j<n){
-            if(nums[j]%2) odd_count++;
-            if(odd_count==k){
-                ans++;
-                p++;
-            }
-            if(odd_count>k){
-                ans++;
-                while(i<n && nums[i]%2==0) i++,ans+=p;
-                p=1;
-                odd_count--;
-                i++;
-            }
-            j++;
+       int n=nums.size();
+       vector<int> nxtodd(n,-1);
+       int prev=-1;
+       if(nums[n-1]%2) prev=n-1;
+       for(int i=n-2;i>=0;i--){
+        nxtodd[i]=prev;
+        if(nums[i]%2) prev=i;
+       }
+       int ans=0;
+       int i=0,j=0,cnt=0;
+       for(;j<n && cnt<k;j++) if(nums[j]%2) cnt++;
+       if(cnt<k) return 0;
+       j--;
+       while(j<n){
+        int p=(nxtodd[i]-i+1);
+        if(nums[i]%2) p=1;
+        ans+=p;
+        j++;
+        if(j>=n) break;
+        if(nums[j]%2){
+            if(nums[i]%2) i++;
+            else i=nxtodd[i]+1;
         }
-        while(i<n && nums[i]%2==0) i++,ans+=p;
-        return ans;
+       }
+       return ans;
     }
 };
