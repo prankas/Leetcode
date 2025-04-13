2a128d60e1767a330e655f75c002aef7246c01f5
prankas <126498373+prankas@users.noreply.github.com>
2025-04-13 13:18:41 +0530

    Time: 29 ms (73.68%), Space: 54 MB (89.47%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    string smallestPalindrome(string s) {
-         int n=s.length();
-         map<char,int> mp;
-         for(auto i:s) mp[i]++;
-         char mid;
-         for(auto i:mp){
-            if(i.second % 2 ){ mid=i.first; break; }
-         }
-         string firsthalf="";
-         for(auto i:mp){
-            for(int j=1;j<=i.second/2;j++) firsthalf+=i.first;
-         }
-         sort(firsthalf.begin(),firsthalf.end());
-         string second="";
-         for(int i=firsthalf.length()-1;i>=0;i--) second+=firsthalf[i];
-         if(mid>='a' && mid<='z') return firsthalf+mid+second;
-         return firsthalf+second;
+    string smallestPalindrome(string& s) {
+        vector<int> cnt(26);
+        for(auto& ch : s) cnt[ch-'a']++;
+        for(int i = 0, l = 0, r = s.length() - 1; i < 26; i++) {
+            while(cnt[i] >= 2) {
+                cnt[i] -= 2;
+                s[l++] = s[r--] = i + 'a';
+            }
+        }
+        return s;
     }
 };
